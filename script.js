561229844784ba7e1fb4ff29e769435f1f45bb52
Akiyashii <fantasy12pl@gmail.com>
2025-02-11 00:36:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ var nieMessages = [
 ];
 
 var nieClickCount = 0;
-var takClickCount = 0;
 
 function selectOption(option) {
     if (option === 'Tak') {
@@ -29,7 +28,7 @@ function selectOption(option) {
 function increaseTakButtonSize() {
     var takButton = document.getElementById('Tak-button');
     var currentTakSize = parseFloat(window.getComputedStyle(takButton).fontSize);
-    var newTakSize = currentTakSize * 1.10; // Zwiększ o 10% na kliknięcie
+    var newTakSize = currentTakSize * 1.15; // Zwiększ o 15% na kliknięcie
     takButton.style.fontSize = newTakSize + 'px';
 }
 
@@ -38,9 +37,9 @@ function handleNieClick() {
     nieButton.innerText = nieMessages[nieClickCount % nieMessages.length]; 
     nieClickCount++;
 
-    // Zmniejsz rozmiar przycisku "Nie"
+    // Zmniejsz rozmiar przycisku "Nie" o 15%
     var currentFontSize = parseFloat(window.getComputedStyle(nieButton).fontSize);
-    var newSize = currentFontSize * 0.90; // Zmniejsz o 10% na kliknięcie
+    var newSize = currentFontSize * 0.85; // Zmniejsz o 15% na kliknięcie
     nieButton.style.fontSize = newSize + 'px';
 
     if (newSize <= 5) {
