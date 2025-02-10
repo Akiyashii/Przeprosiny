9924daa84fece6b9faa747969ac73aa9e5d1f33c
Akiyashii <fantasy12pl@gmail.com>
2025-02-10 15:45:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,22 +3,22 @@
 // Function to handle button click events
 function selectOption(option) {
     // Check which option was clicked
-    if (option === 'yes') {
+    if (option === 'Tak') {
         // Flash rainbow colors
         flashRainbowColors(function() {
             document.getElementById('question').style.display = 'none'; // Hide the question
             displayCatHeart(); // Display the cat-heart.gif
         });
-    } else if (option === 'no') {
-        // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
-        // Increase font size of "Yes" button
-        var yesButton = document.getElementById('yes-button');
-        var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
+    } else if (option === 'Nie') {
+        // Change text on the "Nie" button to "Jesteś pewna?"
+        document.getElementById('Nie-button').innerText = 'Jesteś pewna?'; 
+        // Increase font size of "Tak" button
+        var TakButton = document.getElementById('Tak-button');
+        var currentFontSize = window.getComputedStyle(TakButton).getPropertyValue('font-size');
         var newSize = parseFloat(currentFontSize) * 2; // Increase font size by  * 2px
-        yesButton.style.fontSize = newSize + 'px';
+        TakButton.style.fontSize = newSize + 'px';
     } else {
-        // If neither "Yes" nor "No" was clicked, show an alert message
+        // If neither "Tak" nor "Nie" was clicked, show an alert message
         alert('Invalid option!');
     }
 }
